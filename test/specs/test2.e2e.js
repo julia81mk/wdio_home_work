03cb5a75f09186709d525bf2b0f62e2ed3e68b09
julia81mk <julia81mk@gmail.com>
2023-09-02 17:11:44 +0300

    Additional home work WDIO

--- test/specs/test2.e2e.js
+++ test/specs/test2.e2e.js
@@ -1,42 +1,7 @@
 import { expect, browser, $ } from '@wdio/globals'
 
-/*describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`)
-
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
-
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!')
-    })
-})*/
-
 describe('My First Test', () => {
-    /*it(' Test 1 - Perform Login', async () => {
-        let myUrl = 'https://www.saucedemo.com/';
-        await browser.url(myUrl)
-        //await browser.pause(2000)
-        //await expect(browser).toHaveUrl('WWWWWWaucedemo.com')
-        
-
-        await $('#user-name').setValue('standard_user')
-        await $('#password').setValue('secret_sauce')
-        //await $('button[type="submit"]').click()
-        await $('#login-button').click()
-        await browser.pause(2000)
-
-        const productEl = await $('.header_secondary_container>span') 
-        await expect(productEl).toHaveText('Products')
-
-        const shoppCard = await $('#shopping_cart_container>a') 
-        await expect(shoppCard).toExist()
-
-        const itemsAmount = await $$('.inventory_item');
-        await expect(itemsAmount).toBeElementsArrayOfSize({ gte: 1 }); */
-    it('Test 1 - Perform Login', async () => {
+        it('Test 1 - Perform Login', async () => {
         await browser.url(process.env.BASE_URL)
         await $('#user-name').setValue(process.env.USER_NAME)
         await $('#password').setValue(process.env.USER_PASSWORD)
@@ -66,12 +31,8 @@ describe('My First Test', () => {
         await expect(shoppCard).toHaveText("1")
         const CardLink = await $('.shopping_cart_link')
         await CardLink.click()
-        //await browser.pause(2000)
-        //const Product = await $('#item_4_title_link>.inventory_item_name')
         const Product = await $ ('.inventory_item_name')
-        //await expect(Product).toHaveText("Sauce Labs Backpack")
         await expect(Product).toHaveText( itemTitleText )
-        //await expect(Product).toHaveText({containing: itemTitleText })???
         const Remove_item = await $('button[id="remove-sauce-labs-backpack"]')
         await Remove_item.click()
         const shoppingCartBadgeContainer = await $('.shopping_cart_link');
